a078a36116928f042e7670a8e43858e1a37afe06
John Ronis <jronis@gmail.com>
2020-11-17 23:12:26 +1000

    add resource check function and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,6 +20,39 @@ class BasicTestCase(unittest.TestCase):
         reportResults = app.report(resourceDict)
         self.assertEqual(reportResults, "Money:\t$100\nWater:\t100ml\n")
 
+    def test_check_resources_enough(self):
+        drinkDict = {"Water": 60, "Coffee": 16}
+        resourceList = {
+            "Water": [1000, "ml"],
+            "Milk": [500, "ml"],
+            "Coffee": [76, "g"],
+            "Money": [2.5, "$"],
+        }
+        checkResults = app.check_resources(drinkDict, resourceList)
+        self.assertEqual(checkResults, "thank you")
+
+    def test_check_resources_insufficient_water(self):
+        drinkDict = {"Water": 6000, "Coffee": 16}
+        resourceList = {
+            "Water": [1000, "ml"],
+            "Milk": [500, "ml"],
+            "Coffee": [76, "g"],
+            "Money": [2.5, "$"],
+        }
+        checkResults = app.check_resources(drinkDict, resourceList)
+        self.assertEqual(checkResults, "Sorry not enough Water")
+
+    def test_check_resources_insufficient_coffee(self):
+        drinkDict = {"Water": 60, "Coffee": 1600}
+        resourceList = {
+            "Water": [1000, "ml"],
+            "Milk": [500, "ml"],
+            "Coffee": [76, "g"],
+            "Money": [2.5, "$"],
+        }
+        checkResults = app.check_resources(drinkDict, resourceList)
+        self.assertEqual(checkResults, "Sorry not enough Coffee")
+
 
 if __name__ == "__main__":
     unittest.main()
